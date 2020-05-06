80d550c16fc5cfd61fae1fd102dd59ea0a19bc29
Nobuhiro Yamasaki <nobu.cacao1220@gmail.com>
2020-05-06 20:47:03 +0900

    enemies making

---
+++ Assets/Scripts/EnemyThornController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyThornController : MonoBehaviour
+{
+     //変数を作る
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+         //Rigidbody2Dを取得
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other){
+
+    }
+}
