68129f57f6ea7dee823003d3729a8873eb10b070
yamagi208 <scb01169@gmail.com>
2020-05-10 07:11:16 +0900

    重さリセットアイテム実装

---
+++ Assets/Scripts/ItemController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    TargetManager targetManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetManager = GameObject.Find("TargetManager").GetComponent<TargetManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D thisCollision)
+    {
+        if(thisCollision.gameObject.tag == "Character")
+        {
+            targetManager.weight = 1;
+            Destroy(this.gameObject);
+        }
+    }
+}
