a1dd0ce7b515a092b7045ce7ab2f30e90b308b3c
Nobuhiro Yamasaki <nobu.cacao1220@gmail.com>
2020-05-09 11:36:45 +0900

    down floor making 途中

---
+++ Assets/Scripts/FallDownFloor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallDownFloor : MonoBehaviour
+{
+    //　床が落下するまでの時間
+	[SerializeField]
+	private float timeToFall = 1.0f;
+
+      Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+         //Rigidbody2Dを取得
+        rb = GetComponent<Rigidbody2D>();
+       rb.isKinematic = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     public void OnCollisionEnter2D(Collision2D other){
+        if (other.gameObject.tag == "Character"){
+             StartCoroutine("FloorDownCount");
+        }
+     }
+
+    public void OnCollisionExit2D(Collision2D other){
+        if (other.gameObject.tag == "Character"){
+         StopCoroutine("FloorDownCount");
+         Debug.Log("out");
+        }
+    }
+
+      IEnumerator FloorDownCount()
+    {//３秒経過後に回転する
+        yield return new WaitForSeconds(timeToFall);
+         rb.isKinematic = false;
+    }
+}
