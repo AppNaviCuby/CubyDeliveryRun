58d7298da2b75951d1b7e10daddf1d04c355bf54
Nobuhiro Yamasaki <nobu.cacao1220@gmail.com>
2020-05-18 00:10:07 +0900

    nobuStage Making

--- Assets/Scripts/EnemyDeath.cs
+++ Assets/Scripts/EnemyDeath.cs
@@ -9,33 +9,38 @@ public class EnemyDeath : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(deathflag){
+        if (deathflag)
+        {
 
         }
     }
 
-      public void OnTriggerEnter2D(Collider2D other){
-           if (other.gameObject.tag == "Character"){
-       Debug.Log("death");
-       
-        StartCoroutine("DeathRetry");
-         Time.timeScale = 0.0f;
-           }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Character")
+        {
+            Debug.Log("death");
+
+            StartCoroutine("DeathRetry");
+            Time.timeScale = 0.0f;
+        }
     }
 
-    IEnumerator DeathRetry(){
-            //3秒停止
+    IEnumerator DeathRetry()
+    {
+        //3秒停止
 
-            
-             for (int i = 0; i < 30; i++) { yield return null; }
-           SceneManager.LoadScene (SceneManager.GetActiveScene().name);
-           yield break;
+
+        for (int i = 0; i < 30; i++) { yield return null; }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1.0f;
+        yield break;
     }
-        
+
 }
