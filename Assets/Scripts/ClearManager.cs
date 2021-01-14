d8e822352fd5f8e3d9f5cb4a006ac3c00a519680
elucidator174 <61571492+elucidator174@users.noreply.github.com>
2021-01-14 20:41:55 +0900

    セーブ

--- Assets/Scripts/ClearManager.cs
+++ Assets/Scripts/ClearManager.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ClearManager : MonoBehaviour
 {
     public GameObject clearPanel,tutorialPanel;
     AudioController audioController;
-
+   public int stage_num;
     //public GameObject MenuPanel;
 
     // Start is called before the first frame update
     void Start()
     {
+   stage_num= PlayerPrefs.GetInt ("SCORE", 0);
+       Debug.Log(stage_num);
         audioController = GameObject.Find("AudioController").GetComponent<AudioController>();
     }
 
@@ -23,6 +26,8 @@ void MenuConsole()
         MenuPanel.SetActive(true);
     }
     */
+    string[] StageList = new string[15] { "weightStage", "ItemStage", "PushStage", "kuzureruStage", "Stage1"
+    ,"Nobu_Stage1","hiroStage","Stage_naru","Guchi_Stage2","Stage_naru3","Nobu_Stage2","Stage_naru2","Guchi_Stage1","hiro2stage","Guchi_Stage3" };
 
     void OnTriggerEnter2D(Collider2D thisTrigger)
     {
@@ -34,6 +39,18 @@ void MenuConsole()
                 tutorialPanel.SetActive(false);
             }
             clearPanel.SetActive(true);
+             
+              for(int i=0;i<15;i++){
+            if(SceneManager.GetActiveScene().name==StageList[i]){
+                if(stage_num<=i+2){
+                PlayerPrefs.SetInt ("SCORE", i+2);
+            //PlayerPrefsをセーブする         
+            PlayerPrefs.Save ();
+                }
+            Debug.Log(i+2);
+            }
+             
+              }
         }
     
     }
---
+++ Assets/Scripts/StageScript.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageScript : MonoBehaviour {
+
+    public int stage_num; // スコア変数
+    
+    public GameObject two;
+    public GameObject three;
+    public GameObject four;
+    public GameObject five;
+    public GameObject six;
+    public GameObject seven;
+    public GameObject eight;
+    public GameObject nine;
+    public GameObject ten;
+    public GameObject eleven;
+    public GameObject twelve;
+    public GameObject thirteen;
+    public GameObject fourteen;
+    public GameObject fifteen;
+    
+    public GameObject two_;
+    public GameObject three_;
+    public GameObject four_;
+    public GameObject five_;
+    public GameObject six_;
+    public GameObject seven_;
+    public GameObject eight_;
+    public GameObject nine_;
+    public GameObject ten_;
+    public GameObject eleven_;
+    public GameObject twelve_;
+    public GameObject thirteen_;
+    public GameObject fourteen_;
+    public GameObject fifteen_;
+    
+    
+ 
+
+    // Use this for initialization
+    void Start () {
+        //現在のstage_numを呼び出す
+      stage_num= PlayerPrefs.GetInt ("SCORE", 0);                                                                                                                                                                                                                                                                                                                              
+      
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //stage_numが２以上のとき、ステージ２を解放する。以下同様
+        
+            
+   if(Input.GetKey("r")) {
+		stage_num=0;
+	}
+
+        Debug.Log(stage_num);
+       
+        if (stage_num >= 2) {
+            two.SetActive (true);
+            two_.SetActive(false);
+        }
+
+        if (stage_num >= 3) {
+            three.SetActive (true);
+            three_.SetActive(false);
+        
+        }
+
+        if (stage_num >= 4) {
+            four.SetActive (true);
+            four_.SetActive(false);
+        }
+         if (stage_num >= 5) {
+            five.SetActive(true);
+            five_.SetActive(false);
+        }
+
+        if (stage_num >= 6) {
+            six.SetActive (true);
+            six_.SetActive(false);
+        }
+
+        if (stage_num >= 7) {
+            seven.SetActive (true);
+            seven_.SetActive(false);
+        
+        }
+
+        if (stage_num >= 8) {
+            eight.SetActive (true);
+            eight_.SetActive(false);
+        }
+          if (stage_num >= 9) {
+            nine.SetActive(true);
+            nine_.SetActive(false);
+        }
+
+        if (stage_num >=10) {
+            ten.SetActive (true);
+            ten_.SetActive(false);
+        }
+
+        if (stage_num >=11) {
+            eleven.SetActive (true);
+            eleven_.SetActive(false);
+        
+        }
+
+        if (stage_num >= 12) {
+            twelve.SetActive (true);
+            twelve_.SetActive(false);
+        }
+          if (stage_num >= 13) {
+            thirteen.SetActive(true);
+            thirteen_.SetActive(false);
+        }
+
+        if (stage_num >= 14) {
+            fourteen.SetActive (true);
+            fourteen_.SetActive(false);
+        }
+
+        if (stage_num >= 15) {
+            fifteen.SetActive (true);
+            fifteen_.SetActive(false);
+        
+        }
+
+       
+    }
+}
