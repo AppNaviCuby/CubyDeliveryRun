cc5c9658443deb8d3a8f34ee3e9479b3cf5e4fa4
yamagi208 <scb01169@gmail.com>
2021-01-14 23:08:48 +0900

    タイトル、セレクト画面の調整

---
+++ Assets/Scripts/AllClear.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class AllClear : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    public void OnClick(){
+    PlayerPrefs.SetInt ("SCORE", 15);
+    PlayerPrefs.Save ();
+    }
+}
